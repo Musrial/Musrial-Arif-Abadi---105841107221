21d974345ebe5bc1f9b61cebd05fdfa9213e2268
Musrial <105841107221@student.unismuh.ac.id>
2024-06-02 01:49:46 +0800

    Tugas 3

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import { StyleSheet,Text, View } from "react-native";
+import { StyleSheet,Text, View, Image } from "react-native";
 import React from "react";
 const App = () => {
   return (
-    <View style = {{flex : 1}}>
+
+    <View style = {{flex : 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <Text style = {{
+        fontSize: 35,
+        fontWeight: "bold",
+        marginBottom: 20,
+        position: "center",
+        top: 150,
+      }}>
+        Welcome To My App 
+      </Text>
+      <Image 
+          source={require('./assets/Logo Rial Shop.png')} 
+          style={{ width: 150, height: 150, marginBottom: 20, position: "center", top: 200 ,borderRadius: 25}} 
+        />
       <View style = {{
         flex : 1, flexDirection: "row",
         alignItems: "flex-end",
