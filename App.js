d10156342a542d4c6c3eeea081e76b1c9dbebda4
Musrial <105841107221@student.unismuh.ac.id>
2024-06-12 22:04:55 +0800

    Button Component

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import { StyleSheet,Text, View, Image } from "react-native";
 import React from "react";
-const App = () => {
-  return (
+  const ButtonCustom = ({ text, color }) => {
 
+    return (
+      <View style = {{
+        backgroundColor: color, width: 150, height: 80,
+        justifyContent: "center",
+        borderRadius: 25,
+        marginRight: 20,
+        marginBottom: 20,
+      }}>
+        <Text style = {{
+          fontSize: 30, color: "white",
+          textAlign: "center",
+          fontWeight: "bold"
+        }}>
+          {text}
+        </Text>
+      </View>
+    )
+  }
+
+  const App = () => {
+
+    return (
+  
     <View style = {{flex : 1,
       justifyContent: "center",
       alignItems: "center",
@@ -12,52 +34,25 @@ const App = () => {
         fontWeight: "bold",
         marginBottom: 20,
         position: "center",
-        top: 150,
+        top: 185,
       }}>
-        Welcome To My App 
+        Welcome To My App
       </Text>
       <Image 
           source={require('./assets/Logo Rial Shop.png')} 
-          style={{ width: 150, height: 150, marginBottom: 20, position: "center", top: 200 ,borderRadius: 25}} 
+          style={{ width: 150, height: 150, marginBottom: 20, position: "center", top: 250 ,borderRadius: 25}} 
         />
       <View style = {{
         flex : 1, flexDirection: "row",
         alignItems: "flex-end",
         justifyContent : "center",
       }}>
-        <View style = {{
-          backgroundColor: "blue", width: 150, height: 80,
-          justifyContent: "center",
-          borderRadius: 20,
-          marginRight: 20,
-          marginBottom: 20,
-        }}>
-          <Text style={{
-            fontSize: 30, color: "white",
-            textAlign: "center",
-            fontWeight: "bold"
-          }}>
-            Login
-          </Text>
-        </View>
+          <ButtonCustom color = "darkred" text = "Login"/>
+          <ButtonCustom color = "darkblue" text = "Register"/>
 
-        <View style = {{
-          backgroundColor: "blue", width: 150, height: 80,
-          justifyContent: "center",
-          borderRadius: 20,
-          marginLeft: 20,
-          marginBottom: 20,
-        }}>
-        <Text style = {{
-          fontSize: 30, color: "white",
-          textAlign:"center",
-          fontWeight: "bold"
-        }}>
-          Register
-        </Text>
-        </View>
       </View>
     </View>
   )
 }
+  
 export default App
