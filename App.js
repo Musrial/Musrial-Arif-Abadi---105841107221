07f90bb051f432925fdaa063f9c0b71354ccabc2
Musrial <105841107221@student.unismuh.ac.id>
2024-06-14 21:34:41 +0800

    import/export Component

--- App.js
+++ App.js
@@ -1,58 +1,19 @@
-import { StyleSheet,Text, View, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
-  const ButtonCustom = ({ text, color }) => {
+import { AppContent } from "./components/Button";
 
-    return (
-      <View style = {{
-        backgroundColor: color, width: 150, height: 80,
-        justifyContent: "center",
-        borderRadius: 25,
-        marginRight: 20,
-        marginBottom: 20,
-      }}>
-        <Text style = {{
-          fontSize: 30, color: "white",
-          textAlign: "center",
-          fontWeight: "bold"
-        }}>
-          {text}
-        </Text>
-      </View>
-    )
-  }
+const App = () => {
+  return (
+    <view style= {styles.container}>
+      <AppContent/>
+    </view>
+  );
+};
 
-  const App = () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
-    return (
-  
-    <View style = {{flex : 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Text style = {{
-        fontSize: 35,
-        fontWeight: "bold",
-        marginBottom: 20,
-        position: "center",
-        top: 185,
-      }}>
-        Welcome To My App
-      </Text>
-      <Image 
-          source={require('./assets/Logo Rial Shop.png')} 
-          style={{ width: 150, height: 150, marginBottom: 20, position: "center", top: 250 ,borderRadius: 25}} 
-        />
-      <View style = {{
-        flex : 1, flexDirection: "row",
-        alignItems: "flex-end",
-        justifyContent : "center",
-      }}>
-          <ButtonCustom color = "darkred" text = "Login"/>
-          <ButtonCustom color = "darkblue" text = "Register"/>
-
-      </View>
-    </View>
-  )
-}
-  
-export default App
+export default App;
---
+++ components/Button.js
@@ -0,0 +1,56 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+
+const ButtonCustom = ({text, color}) => {
+  return (
+    <View style= {{
+      backgroundColor: color, width: 150, height: 80,
+      justifyContent: "center",
+      borderRadius : 20,
+      marginRight: 20,
+      marginBottom: 20,
+    }}>
+      <Text style= {{
+        fontSize: 30, color: "white",
+        textAlign: "center",
+        fontWeight: "bold",
+      }}>
+        {text}
+      </Text>
+    </View>
+  );
+};
+
+const AppContent = () => {
+  return (
+    <View style= {{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <Text style= {{
+          fontSize: 35,
+          fontWeight: "bold",
+          marginBottom: 20,
+          position: "absolute",
+          top: 125,
+        }}>
+          Welcome To My App
+        </Text>
+        
+        <Image
+          source= {require('../assets/Logo Rial Shop.png')}
+          style= {{ width: 150, height: 150, marginBottom: 20, position: "absolute", top: 200, borderRadius: 25}}/>
+          <View style= {{
+            flex: 1, flexDirection: "row",
+            alignItems: "flex-end",
+            justifyContent: "center"
+          }}>
+            <ButtonCustom color= "darkred" text= "Login"/>
+            <ButtonCustom color= "darkblue" text= "Register"/>
+          </View>
+    </View>
+  );
+}
+
+export {ButtonCustom, AppContent}
