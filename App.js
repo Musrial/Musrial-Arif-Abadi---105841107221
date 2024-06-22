2973e08c79ce92d4203a72f6ef5e8348a9742177
Musrial <105841107221@student.unismuh.ac.id>
2024-06-22 20:02:29 +0800

    Text Input

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StyleSheet, View } from "react-native";
-import React from "react";
-import { AppContent, ButtonCustom } from "./components/Button";
+import { StyleSheet, Text, View, TextInput } from "react-native-web";
+import React from "react" ;
+const TextInputCustom = ({ name, color}) => {
+  return (
+    <TextInput placeholder={` ${name}`} style={styles.input(color)} />
+  );
+}; 
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <ButtonCustom/>
-      <AppContent/>
-    </View>
+      <TextInputCustom name = "Username" color = "darkred"/>
+      <TextInputCustom name = "Email" color = "darkgreen"/>
+      <TextInputCustom name = "Password" color = "darkblue"/>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, justifyContent: "center", alignItems: "center",
   },
+
+  input: (color) => ({
+    height: 50,
+    width: 300,
+    backgroundColor: "skyblue",
+    marginBottom: 10,
+    borderRadius: 10,
+    borderWidth:1,
+    paddingLeft: 10,
+    color: color,
+  }),
 });
+
+
+
 
 export default App;
