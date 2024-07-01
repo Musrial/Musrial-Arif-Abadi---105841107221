b11dfad048ea354fa99ae27ce73eaf88933bf80d
Musrial <105841107221@student.unismuh.ac.id>
2024-07-01 19:01:27 +0800

    Use Font

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import Singup from './components/signUp';
-import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword';
+import { useFonts } from 'expo-font';
+import { View, Text } from 'react-native';
 
-const App = () => {
+export default function App() {
+  const [dapatFont] = useFonts({
+    'MetroBlack': require('./assets/font/Metropolis-Black.otf'),
+    'MetroBold': require('./assets/font/Metropolis-Bold.otf'),
+    'MetroLight': require('./assets/font/Metropolis-Light.otf'),
+    'MetroMedium': require('./assets/font/Metropolis-Medium.otf'),
+    'MetroSemiBold': require('./assets/font/Metropolis-SemiBold.otf'),
+  });
+
+  if (!dapatFont){
+    return <Text>Sedang Memuat...</Text>
+  }
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-       <ForgotPassword />
-    </ScrollView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-
-});
-
-
-
-
-export default App;
-
+    <View style = {{
+      flex : 1, 
+      justifyContent : 'center', 
+      alignItems : 'center'
+    }}>
+      <Text style = {{ fontnFamily : 'MetroBlack', fontSize : 25}}>Font Metropolis Black</Text>
+      <Text style = {{ fontFamily : 'MetroBold', fontSize : 25}}>Font Metropolis Bold</Text>
+      <Text style = {{ fontFamily : 'MetroLight', fontSize : 25}}>Font Metropolis Light</Text>
+      <Text style = {{ fontFamily : 'MetroMedium', fontSize : 25}}>Metropolis Medium</Text>
+      <Text style = {{ fontFamily : 'MetroSemiBold', fontSize : 25}}>Metropolis Semi Bold</Text>
+    </View>
+  )}
