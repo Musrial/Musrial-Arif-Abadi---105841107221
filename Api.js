a843e71fe6c53fc4040a19e8021d277c732dfd73
Musrial <105841107221@student.unismuh.ac.id>
2024-07-29 15:33:07 +0800

    API

--- Api.js
+++ Api.js
@@ -1,30 +1,45 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword';
-import HomeScreen from './components/HomeScreen';
-import MyTabs from './components/MyTabs';
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { Button } from 'react-native-web'
 
+const App = () => {
+  const [formLogin, setForm] = useState({
+    email: '',
+    password: ''
+  })
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+  const onSubmit = () => {
+    if (formLogin.email === 'musrial' && formLogin.password === 'musrial' ) {
+      alert('Login Success')
+    } else {
+      alert('Login Failed')
+    }
+  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-      initialRouteName='Login'>
-        <Stack.Screen name="MyTabs" component={MyTabs} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Form Login</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(hasil) => setForm({ ...formLogin, email: hasil })}
+          value={formLogin.email}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => setForm({ ...formLogin, password: text })}
+          value={formLogin.password}
+        />
+        <View style={{ marginTop: 10 }}>
+          <Button title="Login" onPress={() => onSubmit()}/>
+        </View>
+        <View>
+          <Text>Email: {formLogin.email}</Text>
+          <Text>Password: {formLogin.password}</Text>
+        </View>
+      </View>
+    </View>
+  )
 }
-
-export default App;
-
+export default App
