29da48285ef9f7aed877ae7c4ea8a2ecd2f2b130
Musrial <105841107221@student.unismuh.ac.id>
2024-07-29 16:23:29 +0800

    HOME SCREEN FIX TERBARU

--- App.js
+++ App.js
@@ -1,26 +1,80 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image, TouchableOpacity, TouchableOpacityBase } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import ForgotPassword from './components/ForgotPassword';
-import HomeScreen from './components/HomeScreen';
-import MyTabs from './components/MyTabs';
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeAktif from './assets/Icon/HomeAktif.jpg';
+import HomeInaktif from './assets/Icon/Home.jpg';
+import Shop from './assets/Icon/ShopAktif.jpg'
+import ShopInaktif from './assets/Icon/Shop.jpg'
 
+const Tab = createBottomTabNavigator();
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? HomeAktif : HomeInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? Shop : ShopInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false, tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? Shop : ShopInaktif}
+              style={{ width: 40, height: 40 }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Ke Halaman Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
+}
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      initialRouteName='Login'>
-        <Stack.Screen name="MyTabs" component={MyTabs} />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
